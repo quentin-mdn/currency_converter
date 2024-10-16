ae9c9bef8cfbddb807a2b9e88c704f8ff1d0cdfb
Quentin Modena <qm@anthemis.fr>
2024-10-17 01:26:19 +0200

    Fix manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
         <title>Currency converter</title>
         <meta charSet="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="manifest" href="/manifest.json"/>
         <meta name="theme-color" content="#000000"/>
         {/* Link to the favicon */}
         <link rel="icon" href="/favicon.ico"/>
