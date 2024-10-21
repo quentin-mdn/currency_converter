be1a7ca7c5fb0a83558608fef01b2b2365eb7352
Quentin Modena <qm@anthemis.fr>
2024-10-21 23:20:49 +0200

    Correct PWA

--- next.config.js
+++ next.config.js
@@ -2,6 +2,33 @@ const withPWA = require('next-pwa')({
     dest: 'public', // This is where your service worker will be stored
     register: true,
     skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching: [
+        {
+            // Cache static files like JS, CSS, etc.
+            urlPattern: /\.(?:js|css|html)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'static-resources',
+                expiration: {
+                    maxEntries: 50, // Number of entries to keep in cache
+                    maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for one week
+                },
+            },
+        },
+        {
+            // Cache images
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|ico|woff|woff2|ttf)$/,
+            handler: 'CacheFirst',
+            options: {
+                cacheName: 'images-cache',
+                expiration: {
+                    maxEntries: 100,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+                },
+            },
+        }
+    ]
 });
 
 // Your Next.js configuration options can go here
